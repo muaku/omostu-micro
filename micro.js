e3a0398e77712f11375b73120db4833b91792443
Muaku <muaku12@gmail.com>
2017-09-05 17:15:13 +0900

    MEGA DONE

--- micro.js
+++ micro.js
@@ -20,6 +20,40 @@ const TYPE1 = {
 		TO_INDEX: 6
 	}	
 }
+const TYPE2 = {
+	VAL: 2,
+	LEN: 14,
+	DATA_LEN: 2,
+	/* INDEX AFTER REMOVING 'PRE' BUFFER */
+	DATA_START_INDEX: 2,
+	DATA_END_INDEX: 3
+}
+const TYPE3 = {
+	VAL: 2,
+	LEN: 14,
+	DATA_LEN: 2,
+	/* INDEX AFTER REMOVING 'PRE' BUFFER */
+	DATA_START_INDEX: 2,
+	DATA_END_INDEX: 3
+}
+const TYPE9 = {
+	VAL: 2,
+	LEN: 14,
+	DATA_LEN: 2,
+	/* INDEX AFTER REMOVING 'PRE' BUFFER */
+	DATA_START_INDEX: 2,
+	DATA_END_INDEX: 4
+}
+const TYPE10 = {
+	VAL: 2,
+	LEN: 14,
+	DATA_LEN: 2,
+	/* INDEX AFTER REMOVING 'PRE' BUFFER */
+	DATA_START_INDEX: 2,
+	DATA_END_INDEX: 3
+}
+
+ /* DATA TO PASS TO Fluentd */
 var DATA_TO_FLUENTD = {}
 
 /* FLUENTD CONFIG */
@@ -31,7 +65,7 @@ fluentd.configure("microsensor", {
 })
 
 /* OPEN PORT ON MAC OR RASPI */
-const portNumber = (process.platform == "darwin") ? '/dev/tty.usbserial-A1044Y1Q' : '/dev/ttyUSB0'
+const portNumber = (process.platform == "darwin") ? '/dev/tty.usbserial-A1044Y1Q' : '/dev/ttyUSB1'
 const port = new SerialPort(portNumber, {
 	baudRate: 115200
 }, function(err) {
@@ -46,33 +80,48 @@ port.on("data", function(chunk) {
 	buffer = Buffer.concat([buffer, chunk])
 	var datas = bsplit(buffer,PRE)
 	
-	// data[1] is perfect buffer, around 30-50ms
+	// datas[0] is empty 
 	if(datas.length > 2) {
-		extractData(datas[1])
-		buffer = Buffer.from([])		// clear buffer
+		extractData(datas[1])	// should use child process
 	}
 })
 
 /* EXTRACT data */
 const extractData = function(data) {
 	 var type = data.readUIntBE(0,1)	// GET type
-	
-	 if(type === 1) {
-		var heart = data.slice(TYPE1.HEART.FROM_INDEX, TYPE1.HEART.TO_INDEX ).readInt16BE()
-		var breath = data.slice(TYPE1.BREATH.FROM_INDEX, TYPE1.BREATH.TO_INDEX ).readInt16BE()
 
-		/* DATA TO PASS TO Fluentd */
-		DATA_TO_FLUENTD = {
-			heart: heart,
-			breath: breath
-		}
+	 switch(type) {
+		 case 1:
+			buffer = buffer.slice(TYPE1.LEN)
+			break
+		 case 2:
+			buffer = buffer.slice(TYPE2.LEN)
+			var heart = data.readUIntBE(TYPE2.DATA_START_INDEX, 1)	// Read 1 byte
+			DATA_TO_FLUENTD["heart"] = heart
+			break
+		 case 3:
+			buffer = buffer.slice(TYPE3.LEN)
+			var breath = data.readUIntBE(TYPE3.DATA_START_INDEX, 1)
+			DATA_TO_FLUENTD["breath"] = breath
+			break
+		 case 9:
+			  buffer = buffer.slice(TYPE9.LEN)
+			break
+		 case 10:
+			  buffer = buffer.slice(TYPE10.LEN)
+			break
+		 default:
+		  	console.log("TYPE is out of range")
 	 }
 }
 
-/* RUN EVERY 5 SEC */
-var Job = new CronJob('*/5 * * * * *', function() {
-	console.log("RUN EVERY 5 SEC ", Date())
-	console.log("DATA_TO_FLUENTD: ", DATA_TO_FLUENTD)
-	fluentd.emit("data", DATA_TO_FLUENTD)
-	console.log("*----------------------------------------------* ")
+/* RUN EVERY 10 SEC */
+var Job = new CronJob('*/10 * * * * *', function() {
+	console.log("RUN EVERY 10 SEC ", Date())
+	// IF there is data then send it to fluentd
+	if(typeof(DATA_TO_FLUENTD["heart"]) != "undefined" & typeof(DATA_TO_FLUENTD["breath"]) != "undefined"){
+		console.log("DATA_TO_FLUENTD: ", DATA_TO_FLUENTD)
+		fluentd.emit("micro", DATA_TO_FLUENTD)
+		console.log("*----------------------------------------------* ")
+	}
 }).start()
